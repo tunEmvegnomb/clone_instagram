545d0f9f57f937c60c8c2436b980f9fbb3a31ce9
migdracios <migdracios@gmail.com>
2022-05-04 13:32:49 +0900

    짜깁기 - 프로젝트 바디(피드,모달,푸터) 추가

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-import flask
+from flask import Flask, render_template,request,jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('main.html')
+
+if __name__ == '__main__':
+
+    app.run('0.0.0.0', port=5000, debug=True)
+
+
