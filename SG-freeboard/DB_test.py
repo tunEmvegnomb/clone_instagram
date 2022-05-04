8ddab235fb5e50780cb705afaad09b4a5dba8f79
SG-Marco <rollingpoe@gmail.com>
2022-05-04 17:01:02 +0900

    데이터 형태 구축 샘플

---
+++ SG-freeboard/DB_test.py
@@ -0,0 +1,32 @@
+doc = {
+    "user_id": "Paul",
+    "hashed_password": "Miller",
+    "follow": [1, 2, 3],
+    "follower": [1, 2, 3],
+    "feeds": [
+        {
+            "feed_id": "Bentley",
+            "time": 1973,
+            "url": "http://www.naver.com",
+            "feed_text": "가나다라마바사",
+            "like": 10,
+            "like_ids": [1, 3, 5],
+            "replies": [
+                {
+                    "reply_id": "Rolls Royce",
+                    "reply_text": "오빠 사랑해요"
+                },
+                {
+                    "reply_id": "BMW",
+                    "reply_text": "오빠 사랑해요"
+                },
+                {
+                    "reply_id": "Benz",
+                    "reply_text": "오빠 사랑해요"
+                }
+
+            ]
+
+        }
+    ]
+}
