2b305f1ddc502d6e7793e996bcb34249691dbcd6
migdracios <migdracios@gmail.com>
2022-05-09 18:13:29 +0900

    추가 - 이미지 업로드 API 추가 및 AJAX 를 통해 이미지 연결 성공

--- static/js/main-imageUpload.js
+++ static/js/main-imageUpload.js
@@ -2,44 +2,50 @@ $(document).ready(function () {
     bsCustomFileInput.init()
 })
 
-// function imageUpload() {
-//     let file = $('#file')[0].files[0]
-//
-//
-//     $.ajax({
-//         type: "POST",
-//         url: "/posting",
-//         data: {'file_give': file},
-//         success: function (response) {
-//             console.log('response')
-//         }
-//     });
-// }
-
 function loadFile(input) {
-    // 파일 가져오기
-    const file = input.files[0];
-    console.log('file 속성')
+    let file = $('#file')[0].files[0]
+    let form_data = new FormData()
+    form_data.append("file_give", file)
 
+    console.log(file)
 
-// 새로운 이미지 div 추가
-//     const newImage = document.createElement("img");
-//     newImage.setAttribute("class", "img");
-
-//    이미지 source 가져오기
-//      newImage.src = URL.createObjectURL(file);
-//
-//     console.log(newImage.src)
-
-//    비포어 업로드 모달 닫기
-    const beforeUpload = document.getElementById("beforeUpload");
-    beforeUpload.style.display = "none"
-//    애프터 업로드 모달 열기
-    const afterUpload = document.getElementById("afterUpload");
-    afterUpload.style.display = "block"
-//    애프터 업로드 모달에 이미지 넣기
-    const imageBody = document.getElementsByClassName("imageBody")
-    imageBody.style.backgroundImage = 'url(' + file.src + ')';
-
+    $.ajax({
+        type: "POST",
+        url: "/imageUpload",
+        data: form_data,
+        // json을 배열로 보내려고 해서 생기는 오류 방지
+        contentType: false,
+        processData: false,
+        success: function (response) {
+            console.log(response)
+        }
+    });
 }
+
+// function loadFile(input) {
+//     // 파일 가져오기
+//     const file = input.files[0];
+//     console.log('file 속성')
+//
+//
+// // 새로운 이미지 div 추가
+// //     const newImage = document.createElement("img");
+// //     newImage.setAttribute("class", "img");
+//
+// //    이미지 source 가져오기
+// //      newImage.src = URL.createObjectURL(file);
+// //
+// //     console.log(newImage.src)
+//
+// //    비포어 업로드 모달 닫기
+//     const beforeUpload = document.getElementById("beforeUpload");
+//     beforeUpload.style.display = "none"
+// //    애프터 업로드 모달 열기
+//     const afterUpload = document.getElementById("afterUpload");
+//     afterUpload.style.display = "block"
+// //    애프터 업로드 모달에 이미지 넣기
+//     const imageBody = document.getElementsByClassName("imageBody")
+//     imageBody.style.backgroundImage = 'url(' + file.src + ')';
+//
+// }
 
