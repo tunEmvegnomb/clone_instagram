08fe4d7e3532a018dce612f740a47479c043d3fa
migdracios <migdracios@gmail.com>
2022-05-09 17:23:27 +0900

    시도 - 새피드 모달 추가 이미지를 받아오면, 해당 이미지를 받아서 모달을 바꿔주는 기능 시도

--- app.py
+++ app.py
@@ -20,12 +20,12 @@ def show_login_page():
     return render_template('login.html')
 
 # 이미지 업로드 API
-@app.route('/imageUpload')
+@app.route('/imageUpload', methods=['POST'])
 def input_image():
     # 사용자 요청 : 이미지 파일
     file_receive = request.files['file_give']
     # API 처리 : 파일을 static/img 에 저장
-    save_to = 'static/mypicture.jpg'
+    save_to = 'static/img/mypicture.jpg'
     file_receive.save(save_to)
     # 응답데이터 : 결과 성공 / 이미지 업로드 성공 메시지 출력
     return jsonify({'result':'success', 'msg': '이미지 업로드에 성공했습니다.'})
