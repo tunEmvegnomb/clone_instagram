565fc96deb26e8acb426860871ce1a206f32460d
migdracios <migdracios@gmail.com>
2022-05-09 18:51:17 +0900

    추가 - 애프터업로드 모달 다음 버튼 기능 구현

--- static/js/main-imageUpload.js
+++ static/js/main-imageUpload.js
@@ -18,10 +18,12 @@ function loadFile(input) {
         processData: false,
         success: function (response) {
             console.log(response)
-            let filename = response['filename']
-            let imageBody = document.getElementsByClassName("imageBody")
+            const filename = response['filename']
+            const imageBody = document.getElementsByClassName("imageBody")
+            const post_image = document.getElementsByClassName("post_image")
             // 애프터 업로드 모달 이미지바디에 사진 주소 변경하기
             imageBody[0].style.backgroundImage = "url(/" + filename + ")"
+            post_image[0].style.backgroundImage = "url(/" + filename + ")"
             //    비포어 업로드 모달 닫기
             const beforeUpload = document.getElementById("beforeUpload");
             beforeUpload.style.display = "none"
@@ -33,30 +35,11 @@ function loadFile(input) {
     });
 }
 
-// function loadFile(input) {
-//     // 파일 가져오기
-//     const file = input.files[0];
-//     console.log('file 속성')
-//
-//
-// // 새로운 이미지 div 추가
-// //     const newImage = document.createElement("img");
-// //     newImage.setAttribute("class", "img");
-//
-// //    이미지 source 가져오기
-// //      newImage.src = URL.createObjectURL(file);
-// //
-// //     console.log(newImage.src)
-//
-//    비포어 업로드 모달 닫기
-// const beforeUpload = document.getElementById("beforeUpload");
-// beforeUpload.style.display = "none"
-// //    애프터 업로드 모달 열기
-// const afterUpload = document.getElementById("afterUpload");
-// afterUpload.style.display = "block"
-// //    애프터 업로드 모달에 이미지 넣기
-//     const imageBody = document.getElementsByClassName("imageBody")
-//     imageBody.style.backgroundImage = 'url(' + file.src + ')';
-//
-// }
+function show_completePost() {
+    const afterUpload = document.getElementById("afterUpload");
+    afterUpload.style.display = "none"
+    const completePost = document.getElementById("completePost");
+    completePost.style.display = "block"
+
+}
 
