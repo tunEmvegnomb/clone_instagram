ab84ae4b993f471901cb1a1c494048a8677c8207
SG-Marco <rollingpoe@gmail.com>
2022-05-09 13:46:04 +0900

    수정-클래스, 아이디

--- static/js/main-storycardsShowMore.js
+++ static/js/main-storycardsShowMore.js
@@ -1,19 +1,23 @@
-const story_data_id_increment = document.getElementsByClassName("content_content");
-const seemore_id_increment = document.getElementsByClassName("seeemore");
+
+
+
+
+const story_data_id_increment = document.getElementsByClassName("storycards_bottom_story_data");
+const story_morebtn_id_increment = document.getElementsByClassName("storycards_bottom_story_morebtn");
 
 function isEllipsisActive(e) {
     return (e.offsetWidth < e.scrollWidth);
 }
 
-function seeeemore(id) {
+function show_me_more(id) {
     document.getElementById(id).style.display = "None";
-    document.getElementById("sibal"+id.slice(-1)).style.whiteSpace = "unset";
+    document.getElementById("story_data"+id.slice(-1)).style.whiteSpace = "unset";
 }
-for (let i = 0; i < sibal_id_increment.length; i++) {
-    sibal_id_increment[i].id = "sibal" + i;
-    seemore_id_increment[i].id = "seemore" + i;
+for (let i = 0; i < story_data_id_increment.length; i++) {
+    story_data_id_increment[i].id = "story_data" + i;
+    story_morebtn_id_increment[i].id = "btn_show_more" + i;
 
-    if (isEllipsisActive(document.getElementById("sibal" + i)) != true) {
-        document.getElementById("seemore" + i).style.display = "None";
+    if (isEllipsisActive(document.getElementById("story_data" + i)) != true) {
+        document.getElementById("btn_show_more" + i).style.display = "None";
     }
 }
