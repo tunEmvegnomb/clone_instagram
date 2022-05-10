9255ebfa8290421749a6b353e7e2b87cd0432208
migdracios <migdracios@gmail.com>
2022-05-10 10:02:00 +0900

    추가 - 컴플릿 모달 공유하기 버튼 API 추가

--- app.py
+++ app.py
@@ -124,5 +124,23 @@ def input_image():
     # 응답데이터 : 결과 성공 / 이미지 업로드 성공 메시지 /
     return jsonify({'result':'success', 'msg': '이미지 업로드에 성공했습니다.', 'filename': save_to})
 
+@app.route('/feedUpload', methods=['POST'])
+def upload_feed():
+    id_receive = request.form['id_give']
+    content_receive = request.form['content_give']
+
+    doc = {
+        'id': id_receive,
+        'content' : content_receive
+    }
+
+    db.feeds.insert_one(doc)
+
+    return  jsonify({'result': 'success', 'msg': '새 피드를 등록했습니다 =>'})
+
+@app.route('/mypage')
+def show_mypage():
+    return render_template('mypage.html')
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
