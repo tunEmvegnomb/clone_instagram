b0c54e6e5209a78a0bde5a92d0cd66b7b1493027
migdracios <migdracios@gmail.com>
2022-05-11 10:49:31 +0900

    수정 - 하나의 Ajax에서 불러올 수 있도록 변수 타입 변경

--- static/js/main-getFeed.js
+++ static/js/main-getFeed.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 
 function get_feed() {
     $("#feeds").empty()
-    $("#feedModal").empty()
+    $(".mw").empty()
 
     $.ajax({
         type: "GET",
@@ -14,19 +14,16 @@ function get_feed() {
         contentType: false,
         processData: false,
         success: function (response) {
-            console.log(response)
             alert('피드 불러오기!')
-            let data = response['result']
+            data = response['result']
             console.log(data)
+
             for (i = 0; i < data.length; i++) {
-                // 임시로 값 선언. 맞는 값 아님
-                let name = data[i]['author_id']
-                let like_cnt = data[i]['like_post_count']
-                let content = data[i]['article']
-                let post_image = data[i]['post_img']
-                let post_id = data[i]['post_id']
-                console.log(name)
-                // let num = data['index_num']
+                name = data[i]['author_id']
+                like_cnt = data[i]['like_post_count']
+                content = data[i]['article']
+                post_image = data[i]['post_img']
+                post_id = data[i]['post_id']
 
                 let temp_html = `<div class="wrap_storycards">
                                     <!--            4. 스토리카드 상단 div -->
@@ -48,7 +45,7 @@ function get_feed() {
                                             <!--                    6. 스토리카드 상단 옵션 div -->
                                             <div class="storycards_top_option">
                         
-                                                <i class="fa-solid fa-ellipsis s40 option-modal${i+1}" onclick="showOptionModal(${i+1})"></i>
+                                                <i class="fa-solid fa-ellipsis s40 option-modal${i + 1}" onclick="showOptionModal(${i + 1})"></i>
                         
                                             </div>
                                         </div>
@@ -56,14 +53,14 @@ function get_feed() {
                                     <!--            4. 스토리카드 중단 div -->
                                     <div class="storycards_middle">
                                         <!--                5. 스토리카드 중단 캐러셀 div -->
-                                        <div id="carousel-${i+1}" class="carousel slide" data-bs-ride="carousel">
+                                        <div id="carousel-${i + 1}" class="carousel slide" data-bs-ride="carousel">
                                             <!--                    캐러셀 인디케이터 -->
                                             <div class="carousel-indicators">
-                                                <button type="button" data-bs-target="#carousel-${i+1}" data-bs-slide-to="0"
+                                                <button type="button" data-bs-target="#carousel-${i + 1}" data-bs-slide-to="0"
                                                         class="active" aria-current="true" aria-label="Slide 1"></button>
-                                                <button type="button" data-bs-target="##carousel-${i+1}" data-bs-slide-to="1"
+                                                <button type="button" data-bs-target="##carousel-${i + 1}" data-bs-slide-to="1"
                                                         aria-label="Slide 2"></button>
-                                                <button type="button" data-bs-target="##carousel-${i+1}" data-bs-slide-to="2"
+                                                <button type="button" data-bs-target="##carousel-${i + 1}" data-bs-slide-to="2"
                                                         aria-label="Slide 3"></button>
                         
                                             </div>
@@ -88,13 +85,13 @@ function get_feed() {
                         
                                             <!--                7. 스토리카드 중단 이미지 넘기기 아이콘 div -->
                                             <div class="storycards_middle_next"
-                                                 data-bs-target="#carousel-${i+1}"
+                                                 data-bs-target="#carousel-${i + 1}"
                                                  data-bs-slide="next">
                                                 <i class="fa-solid fa-circle-chevron-right btn_next"></i>
                                             </div>
                                             <!--                7. 스토리카드 중단 이미지 되돌리기 아이콘 div -->
                                             <div class="storycards_middle_prev"
-                                                 data-bs-target="#carousel-${i+1}"
+                                                 data-bs-target="#carousel-${i + 1}"
                                                  data-bs-slide="prev">
                                                 <i class="fa-solid fa-circle-chevron-left btn_prev"></i>
                                             </div>
@@ -179,10 +176,10 @@ function get_feed() {
                                                 <!--                    7. 스토리카드 하단 스토리 아이디 div -->
                                                 <div class="storycards_bottom_story_user">${name}</div>
                                                 <!--                    7. 스토리카드 하단 스토리 내용 div -->
-                                                <div class="storycards_bottom_story_data feed_desc${i+1}">${content}
+                                                <div class="storycards_bottom_story_data feed_desc${i + 1}">${content}
                                                 </div>
                                                 <!--                    7. 스토리카드 하단 스토리 더보기 버튼-->
-                                                <div class="storycards_bottom_story_morebtn seeMore_btn${i+1}" onclick="show_me_more(${i+1})">더 보기</div>
+                                                <div class="storycards_bottom_story_morebtn seeMore_btn${i + 1}" onclick="show_me_more(${i + 1})">더 보기</div>
                                             </div>
                                             <!--                    6. 스토리카드 하단 스토리 댓글보기 div -->
                                             <div class="storycards_bottom_story_viewcomment">댓글 8개 모두 보기</div>
@@ -213,74 +210,88 @@ function get_feed() {
                                 </div>`
                 $("#feeds").append(temp_html)
 
-                let temp_html2 = `<div id="feedModal" class="modalBackground">
-                                    <div class="modalWindow">
-                                        <div id="feedModalPop" class="modalPopup">
-                                            <div class="feedImage"></div>
-                                            <div class="feedContents">
-                                                <div class="feedProfile">
-                                                    <div>
-                                                        <div class="profile_icon"></div>
-                                                        <span>${name}</span>
-                                                        <i class="fa-solid fa-ellipsis s40"></i>
-                                                    </div>
-                                
+
+            }
+        }
+    });
+
+    $.ajax({
+        type: "GET",
+        url: "/getFeed",
+        data: {},
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: function (response) {
+            for (i = 0; i < data.length; i++) {
+
+
+            }
+            console.log('모달 동적 붙이기')
+            let temp_html2 = `<div id="feedModalPop" class="modalPopup">
+                                <div class="feedImage"></div>
+                                <div class="feedContents">
+                                    <div class="feedProfile">
+                                        <div>
+                                            <div class="profile_icon"></div>
+                                            <span>${name}</span>
+                                            <i class="fa-solid fa-ellipsis s40"></i>
+                                        </div>
+                    
+                                    </div>
+                                    <div class="feedInnerContent">
+                                        <div>
+                                            <div class="profile_icon"></div>
+                                            <div class="content_inner">
+                                                <span class="profile_name">${name}</span>
+                                                <span>${content}</span>
+                                                <p style="color: #8e8e8e; margin-top: 10px">5일</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="feedReaction">
+                                        <div>
+                                            <div class="reaction_icons">
+                                                <button><i class="fa-regular fa-heart s24"></i></button>
+                                                <button><i class="fa-regular fa-comment s24"></i></button>
+                                                <button><i class="fa-regular fa-paper-plane s24"></i></button>
+                                                <button style="width: 400px; padding-left: 310px"><i class="fa-regular fa-bookmark s24"></i>
+                                                </button>
+                                            </div>
+                                            <div class="storycards_bottom_likes_flex">
+                                                <div class="storycards_bottom_likes_icons">
+                                                    <div class="storycards_bottom_likes_icons_icon profile_mona likes_icon1"></div>
+                                                    <div class="storycards_bottom_likes_icons_icon profile_love likes_icon2"></div>
+                                                    <div class="storycards_bottom_likes_icons_icon profile_van likes_icon3"></div>
                                                 </div>
-                                                <div class="feedInnerContent">
-                                                    <div>
-                                                        <div class="profile_icon"></div>
-                                                        <div class="content_inner">
-                                                            <span class="profile_name">${name}</span>
-                                                            <span>${content}</span>
-                                                            <p style="color: #8e8e8e; margin-top: 10px">5일</p>
-                                                        </div>
-                                                    </div>
+                                                <div class="storycards_bottom_likes_span">
+                                                    <span class="like_user">GoldVedio</span>님<span class="like_count"> 외 ${like_cnt}명</span>이 좋아합니다
                                                 </div>
-                                                <div class="feedReaction">
-                                                    <div>
-                                                        <div class="reaction_icons">
-                                                            <button><i class="fa-regular fa-heart s24"></i></button>
-                                                            <button><i class="fa-regular fa-comment s24"></i></button>
-                                                            <button><i class="fa-regular fa-paper-plane s24"></i></button>
-                                                            <button style="width: 400px; padding-left: 310px"><i class="fa-regular fa-bookmark s24"></i>
-                                                            </button>
-                                                        </div>
-                                                        <div class="storycards_bottom_likes_flex">
-                                                            <div class="storycards_bottom_likes_icons">
-                                                                <div class="storycards_bottom_likes_icons_icon profile_mona likes_icon1"></div>
-                                                                <div class="storycards_bottom_likes_icons_icon profile_love likes_icon2"></div>
-                                                                <div class="storycards_bottom_likes_icons_icon profile_van likes_icon3"></div>
-                                                            </div>
-                                                            <div class="storycards_bottom_likes_span">
-                                                                <span class="like_user">GoldVedio</span>님<span class="like_count"> 외 ${like_cnt}명</span>이 좋아합니다
-                                                            </div>
-                                                        </div>
-                                                        <div class="reaction_date">5일전</div>
-                                                    </div>
-                                                    <div class="storycards_bottom_comment reaction_comment">
-                                                        <div class="storycards_bottom_comment_flex" style="margin-left: 5px">
-                                                            <div class="storycards_bottom_comment_emoji">
-                                                                <i class="fa-regular fa-face-smile s24"></i>
-                                                            </div>
-                                                            <div class="storycards_bottom_comment_input">
-                                                                <input aria-label="입력 댓글" autocapitalize="none"
-                                                                       placeholder="댓글 달기..." type="text" style="width: 400px">
-                                                            </div>
-                                                            <div class="storycards_bottom_comment_postbtn">
-                                                                <a>게시</a>
-                                                            </div>
-                                                        </div>
-                                                    </div>
+                                            </div>
+                                            <div class="reaction_date">5일전</div>
+                                        </div>
+                                        <div class="storycards_bottom_comment reaction_comment">
+                                            <div class="storycards_bottom_comment_flex" style="margin-left: 5px">
+                                                <div class="storycards_bottom_comment_emoji">
+                                                    <i class="fa-regular fa-face-smile s24"></i>
+                                                </div>
+                                                <div class="storycards_bottom_comment_input">
+                                                    <input aria-label="입력 댓글" autocapitalize="none"
+                                                           placeholder="댓글 달기..." type="text" style="width: 400px">
+                                                </div>
+                                                <div class="storycards_bottom_comment_postbtn">
+                                                    <a>게시</a>
                                                 </div>
                                             </div>
                                         </div>
-                                        <div>
-                                            <i class="fa-solid fa-xmark close" onclick="close_feedModal()"></i>
-                                        </div>
                                     </div>
-                                </div>`
-                $("#feedModal").append(temp_html2)
-            }
+                                </div>
+                            </div>
+                            <div>
+                                <!--        화면 닫기 아이콘-->
+                                <i class="fa-solid fa-xmark close" onclick="close_feedModal()"></i>
+                            </div>`
+            $(".mw").append(temp_html2)
         }
     });
 }
